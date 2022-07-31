0ff0e98807b456759d4353f56d3c18e54fc0db6f
Scoobydowsky <tomas.woytkowiak1505@gmail.com>
2022-07-31 15:24:43 +0200

    created layout

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+use Scooby\VwEqipmentDecoder\ControlerPL;
+
+require "vendor/autoload.php";
+$selectLang = <<<LANG
+SELECT LANG:
+1 : POLSKI
+2 : ENGLISH
+LANG.PHP_EOL;
+
+echo $selectLang;
+$lang= readline("Choose: ");
+switch ($lang){
+    case 1 :  $controler= new ControlerPL(); break;
+    case 2 :  $controler= new ControlerEN(); break;
+}
+echo $controler->Header();
+do{
+    $code = $controler->GetCode();
+    echo $controler->SearchCode($code);
+    $another = $controler->AskToAnotherCode();
+}while($another == true);
+
