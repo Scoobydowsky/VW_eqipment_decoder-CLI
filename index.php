1de77a26d748cd77fcd9df06168c1aeeabd7fe65
Scoobydowsky <tomas.woytkowiak1505@gmail.com>
2022-08-01 21:51:25 +0200

    Merge branch 'languge_polish_class' of /home/scooby/PhpstormProjects/VW_eqipment_decoder with conflicts.

--- index.php
+++ index.php
@@ -6,18 +6,22 @@ $selectLang = <<<LANG
 SELECT LANG:
 1 : POLSKI
 2 : ENGLISH
-LANG.PHP_EOL;
+LANG. PHP_EOL;
 
 echo $selectLang;
-$lang= readline("Choose: ");
-switch ($lang){
-    case 1 :  $controler= new \Scooby\VwEqipmentDecoder\ControlerPL(); break;
-    case 2 :  $controler= new \Scooby\VwEqipmentDecoder\ControlerEN(); break;
+$lang = readline("Choose: ");
+switch ($lang) {
+    case 1 :
+        $controler = new \Scooby\VwEqipmentDecoder\ControlerPL();
+        break;
+    case 2 :
+        $controler = new \Scooby\VwEqipmentDecoder\ControlerEN();
+        break;
 }
 echo $controler->Header();
-do{
+do {
     $code = $controler->GetCode();
     echo $controler->SearchAndShowCode($code);
     $another = $controler->AskToAnotherCode();
-}while($another == true);
+} while ($another == true);
 
